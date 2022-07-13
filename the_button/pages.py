5f0e9d7d4cc42d1992b51e4e35f5519d0497ca54
figsis <c.figueroa@tinbergen.nl>
2022-07-13 12:10:31 +0200

    n

--- the_button/pages.py
+++ the_button/pages.py
@@ -105,10 +105,7 @@ class Survey(Page):
     form_model = 'player'
     form_fields = []
 
-    def vars_for_template(self):
-        return dict(payoff1_self=self.player.participant.vars["payoff1_self"],
-                    payoff2_self=self.participant.vars["payoff2_self"],
-                    store_time = self.player.store_time)
+
 
     def is_displayed(self):
         player = self.player
